b0c0915d942737085c573d0fca4a716e2006d507
OksanaDenysenko <oksana.krivopisha@gmail.com>
2024-07-17 13:23:15 +0300

    added commentsÐ,Ð formattedÐ, replaced array() with х[]

--- myhomework2.php
+++ myhomework2.php
@@ -1,15 +1,18 @@
 <?php
 
-// не звертайте на цю функцію уваги
-// вона потрібна для того щоб правильно зчитати вхідні дані
-function readHttpLikeInput() {
-    $f = fopen( 'php://stdin', 'r' );
+/**
+ * This function simulates reading data from standard input in a format that resembles an HTTP request.
+ * @return string
+ */
+function readHttpLikeInput()
+{
+    $f = fopen('php://stdin', 'r');
     $store = "";
     $toread = 0;
-    while( $line = fgets( $f ) ) {
+    while ($line = fgets($f)) {
         $store .= preg_replace("/\r/", "", $line);
-        if (preg_match('/Content-Length: (\d+)/',$line,$m))
-            $toread=$m[1]*1;
+        if (preg_match('/Content-Length: (\d+)/', $line, $m))
+            $toread = $m[1] * 1;
         if ($line == "\r\n")
             break;
     }
@@ -20,33 +23,35 @@ function readHttpLikeInput() {
 
 $contents = readHttpLikeInput();
 
-function parseTcpStringAsHttpRequest($string) {
+/**
+ * This function is designed to parse a string received and convert it into a data structure representing an HTTP request.
+ * @param $string - a string received over a TCP connection
+ * @return array - a data structure representing an HTTP request
+ */
+function parseTcpStringAsHttpRequest($string)
+{
     //$substring = explode(PHP_EOL, $string);
-    $substring = explode("\n",$string);
-    $method_and_uri = explode(" ",$substring[0]);
-    $method=$method_and_uri[0];
-    $uri=$method_and_uri[1];
-    $headers=[];
+    $substrings = explode("\n", $string);
+    $method_and_uri = explode(" ", $substrings[0]);
+    $method = $method_and_uri[0];
+    $uri = $method_and_uri[1];
+    $headers = [];
 
-//    for($i=1; $i<count($substring)-2; $i++){
-//        $value_array=explode(":",$substring[$i]);
-//        $headers[$value_array[0]]=$value_array[1];
-//    }
-    foreach ($substring as $key => $value) {
-        if ($key > 0 && $key < count($substring) - 2) { // Перевірка індексу
+    foreach ($substrings as $key => $value) {
+        if ($key > 0 && $key < count($substrings) - 2) { // Перевірка індексу
             $value_array = explode(":", $value);
             $headers[$value_array[0]] = $value_array[1];
         }
     }
 
-    $body=$substring[count($substring)-1];
+    $body = $substrings[count($substrings) - 1];
 
-    return array(
+    return [
         "method" => $method,
         "uri" => $uri,
         "headers" => $headers,
         "body" => $body,
-    );
+    ];
 }
 
 $http = parseTcpStringAsHttpRequest($contents);
