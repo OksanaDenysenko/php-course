69734989acf7eb5514e03a455cad0d3f604030c6
OksanaDenysenko <oksana.krivopisha@gmail.com>
2024-08-19 17:52:11 +0300

    rename myhomework6.php

--- myhomework6.php
+++ myhomework6.php
@@ -6,15 +6,20 @@
     <title>Document</title>
 </head>
 <body>
+
 <?php
-if (!file_exists("counter.txt")) {
+
+if (!file_exists("counter.txt")) { // create a file for the counter
     file_put_contents("counter.txt", "0");
 }
-$count = file_get_contents("counter.txt");
+
+$count = file_get_contents("counter.txt"); // increase the counter with each visit to the page
 $count++;
+
 file_put_contents("counter.txt", $count);
 echo $count;
 ?>
+
 </body>
 </html>
 
